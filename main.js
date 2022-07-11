c4ffa1eff1044e55ca04061e302d7015cdce6e2b
benjamin zarate <benjaminzaratecaceres03@gmail.com>
2022-07-11 20:18:13 -0300

    switch

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
+let elegirCuotas = parseInt(prompt("¿En cuantas cuotas realizara el pago?"));
 
-
-
-let ingreseNumero = parseInt(prompt("Ingrese un numero"));
-
-for(let i = 1; i <= 10; i++){
-    let resultado = ingreseNumero * i;
-   console.log(`el numero ${ingreseNumero} X ${i} = ${resultado}`);
+while(elegirCuotas != "ESC"){
+    switch(elegirCuotas){
+        case 3:
+            if(elegirCuotas ==3){
+                alert("El valor de cada cuota es de 58USD");
+                break;
+            }
+        case 6:
+            if(elegirCuotas ==6){
+                alert("El valor de cada cuota es de 63USD");
+                break;
+            }
+            
+        case 12:
+            if(elegirCuotas ==12){
+                alert("El valor de cada cuota es de 68USD");
+                break;
+            }
+            
+        case 18:
+            if(elegirCuotas ==18){
+                alert("El valor de cada cuota es de 73USD");
+                break;
+            }
+            
+        default:
+            alert("Cantidad de cuotas incorrecto");
+            break;
+    }
 }
