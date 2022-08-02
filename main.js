837fee670639eded436568a5e691e9780d5f3927
benjamin zarate <benjaminzaratecaceres03@gmail.com>
2022-08-02 11:23:50 -0300

    carrito

--- main.js
+++ main.js
@@ -79,16 +79,33 @@ console.log(`El total a pagar es ${totalCarrito}`);
 
 //stock
 let stockProductos = [
-        {id: 1, nombre: "UnMes", precio: 200, img:'./img/MES 1.png'},
-        {id: 2, nombre: "DosMeses", precio: 260, img: './img/two months.png'},
-        {id: 3, nombre: "TresMeses", precio: 320, img: './img/three.png'},
-        {id: 4, nombre: "SeisMeses", precio: 400, img: './img/six.png'},
-        {id: 5, nombre: "NueveMeses", precio: 480, img: './img/nine.png'},
-        {id: 6, nombre: "DoceMeses", precio: 560, img: './img/twuelve months.png'},
+        {id: 1, nombre: "UnMes", precio: 200, img:'../img/MES1.png'},
+        {id: 2, nombre: "DosMeses", precio: 260, img: '../img/twomonths.png'},
+        {id: 3, nombre: "TresMeses", precio: 320, img: '../img/three.png'},
+        {id: 4, nombre: "SeisMeses", precio: 400, img: '../img/six.png'},
+        {id: 5, nombre: "NueveMeses", precio: 480, img: '../img/nine.png'},
+        {id: 6, nombre: "DoceMeses", precio: 560, img: '../img/twuelvemonths.png'},
 ];
 
 let carrito = [];
 
+const contenedorProductos = document.querySelector(".grilla")
+
+stockProductos.forEach((producto)=>{
+        const div = document.createElement("div")
+        div.classList.add("producto")
+        div.innerHTML = ` 
+        <img src=${producto.img} alt="">
+        <p class= "precioProducto">$${producto.precio}</p>
+        <button id= "agregar${producto.id}" class= "boton-agregar">COMPRAR<i class= "fas-fa-shopping-cart"></i></button>
+        `
+        contenedorProductos.appendChild(div)
+
+        const boton = document.getElementById(`agregar${producto.id}`)
+        boton.addEventListener("click", () =>{
+                agregarCarrito(producto.id)
+        })
+})
 
 const agregarCarrito = (prodId) => {
         const item = stockProductos.find ((prod) => prod.id === prodId);
