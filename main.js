c44c67e7338d41f2cff6c6f1a18abe2b194720b1
benjamin zarate <benjaminzaratecaceres03@gmail.com>
2022-08-03 01:45:44 -0300

    carritoss

--- main.js
+++ main.js
@@ -79,29 +79,34 @@ console.log(`El total a pagar es ${totalCarrito}`);
 
 //stock
 let stockProductos = [
-        {id: 1, nombre: "UnMes", precio: 200, img:'../img/MES1.png'},
-        {id: 2, nombre: "DosMeses", precio: 260, img: '../img/twomonths.png'},
-        {id: 3, nombre: "TresMeses", precio: 320, img: '../img/three.png'},
-        {id: 4, nombre: "SeisMeses", precio: 400, img: '../img/six.png'},
-        {id: 5, nombre: "NueveMeses", precio: 480, img: '../img/nine.png'},
-        {id: 6, nombre: "DoceMeses", precio: 560, img: '../img/twuelvemonths.png'},
+        {id: 1, nombre: "UnMes", precio: 200, cantidad: 1, img:'../img/MES1.png'},
+        {id: 2, nombre: "DosMeses", precio: 260, cantidad: 1, img: '../img/twomonths.png'},
+        {id: 3, nombre: "TresMeses", precio: 320, cantidad: 1, img: '../img/three.png'},
+        {id: 4, nombre: "SeisMeses", precio: 400, cantidad: 1, img: '../img/six.png'},
+        {id: 5, nombre: "NueveMeses", precio: 480, cantidad: 1, img: '../img/nine.png'},
+        {id: 6, nombre: "DoceMeses", precio: 560, cantidad: 1, img: '../img/twuelvemonths.png'},
 ];
 
 let carrito = [];
 
+//variables
 const contenedorProductos = document.querySelector(".grilla")
 
+const contenedorCarrito = document.querySelector(".carrito-contenedor")
+
+//funciones
 stockProductos.forEach((producto)=>{
         const div = document.createElement("div")
         div.classList.add("producto")
         div.innerHTML = ` 
-        <img src=${producto.img} alt="">
+        <img class= "imageCard" src=${producto.img} alt="">
         <p class= "precioProducto">$${producto.precio}</p>
-        <button id= "agregar${producto.id}" class= "boton-agregar">COMPRAR<i class= "fas-fa-shopping-cart"></i></button>
+        <button id= "agregar${producto.id}" class= "boton-agregar">COMPRAR <i class="fas fa-shopping-cart"></i></button>
         `
         contenedorProductos.appendChild(div)
 
         const boton = document.getElementById(`agregar${producto.id}`)
+
         boton.addEventListener("click", () =>{
                 agregarCarrito(producto.id)
         })
@@ -110,9 +115,33 @@ stockProductos.forEach((producto)=>{
 const agregarCarrito = (prodId) => {
         const item = stockProductos.find ((prod) => prod.id === prodId);
         carrito.push(item);
+        actualizarCarrito();
         console.log(carrito);
 }
 
+const eliminarDelCarrito = (prodId) => {
+        const item = carrito.find((prod) => prod.id === prodId)
+        const indice = carrito.indexOf(item)
+        carrito.splice(indice, 1)
+        actualizarCarrito();
+}
+
+const actualizarCarrito = () => {
+        contenedorCarrito.innerHTML = ""
+
+
+        carrito.forEach((prod) => {
+                const div = document.createElement("div")
+                div.classList.add("productoEnCarrito")
+                div.innerHTML = `
+                <p>Nombre: ${prod.nombre}</p>
+                <p>Precio: ${prod.precio}</p>
+                <p>Cantidad: <span id= "cantidad">${prod.cantidad}</span></p>
+                <button onclick = "eliminarDelCarrito(${prod.id})" class= "boton-eliminar"><i class= "fas fa-trash-alt"></i></button>
+                `
+                contenedorCarrito.appendChild(div)
+})
+}
 
 
 
